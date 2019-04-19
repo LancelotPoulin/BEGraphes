fdb9f1d833cca7b61c580efb0b882f1813413afb
M. Gantet Jeremie <jgantet@insa-08113>
2019-04-19 18:07:32 +0200

    Algorithme de Dijkstra fonctionel et class Label complète.

--- src/main/org/insa/algo/shortestpath/DijkstraAlgorithm.java
+++ src/main/org/insa/algo/shortestpath/DijkstraAlgorithm.java
@@ -1,5 +1,15 @@
 package org.insa.algo.shortestpath;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
+import org.insa.algo.AbstractSolution.Status;
+import org.insa.algo.utils.BinaryHeap;
+import org.insa.graph.Arc;
+import org.insa.graph.Graph;
+import org.insa.graph.Node;
+import org.insa.graph.Path;
+
 public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
     public DijkstraAlgorithm(ShortestPathData data) {
@@ -9,8 +19,92 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
     @Override
     protected ShortestPathSolution doRun() {
         ShortestPathData data = getInputData();
+        Graph graph = data.getGraph();
+        
+        final int nbNodes = graph.size();
+        
+        // Initialize array of predecessors.
+        Arc[] predecessorArcs = new Arc[nbNodes];
+        
+        BinaryHeap<Label> tas = new BinaryHeap<Label>();
+        
+        
+        ArrayList<Label> TabLab = new ArrayList<Label>(); 
+        
+        for (int i = 0; i<nbNodes; i++) {
+        	TabLab.add(new Label(i, false, Double.POSITIVE_INFINITY, null));  
+        }
+        TabLab.get(data.getOrigin().getId()).setCost(0);
+        tas.insert(TabLab.get(data.getOrigin().getId()));
         ShortestPathSolution solution = null;
-        // TODO:
+
+        boolean nonMarque = true;
+        while (nonMarque) {
+
+    		Node node = graph.get(tas.findMin().getSommetCourant());
+    		if (node == data.getDestination()) {
+    			nonMarque = false;
+    		}
+    		else {
+    			TabLab.get(node.getId()).setMarque(true);
+            	for (Arc arc: node.getSuccessors()) {
+            		
+                    // Small test to check allowed roads...
+                    if (!data.isAllowed(arc)) {
+                        continue;
+                    }
+                    
+                    if (TabLab.get(arc.getDestination().getId()).getMarque()==false) {
+                    	double oldDist = TabLab.get(arc.getDestination().getId()).getCost();
+                    	double newDist = TabLab.get(node.getId()).getCost() + data.getCost(arc);
+                        if (Double.isInfinite(oldDist) && Double.isFinite(newDist)) {
+                            notifyNodeReached(arc.getDestination());
+                        }
+                    	if (oldDist > newDist) {
+                    		TabLab.get(arc.getDestination().getId()).setCost(newDist); 
+                			
+                    		predecessorArcs[arc.getDestination().getId()] = arc;
+                			
+                    		if (TabLab.get(arc.getDestination().getId()).getVisite()) {
+                    			tas.remove(TabLab.get(arc.getDestination().getId())); 
+                    			tas.insert(TabLab.get(arc.getDestination().getId()));
+                    		} else {
+                    			tas.insert(TabLab.get(arc.getDestination().getId()));
+                    			TabLab.get(arc.getDestination().getId()).setVisite(true);
+                    		}
+                    		
+                    	}
+                    }
+        		}
+        	}
+    		tas.deleteMin();
+    	}
+        
+        
+        // Destination has no predecessor, the solution is infeasible...
+        if (predecessorArcs[data.getDestination().getId()] == null) {
+            solution = new ShortestPathSolution(data, Status.INFEASIBLE);
+        }
+        else {
+
+            // The destination has been found, notify the observers.
+            notifyDestinationReached(data.getDestination());
+
+            // Create the path from the array of predecessors...
+            ArrayList<Arc> arcs = new ArrayList<>();
+            Arc arc = predecessorArcs[data.getDestination().getId()];
+            while (arc != null) {
+                arcs.add(arc);
+                arc = predecessorArcs[arc.getOrigin().getId()];
+            }
+
+            // Reverse the path...
+            Collections.reverse(arcs);
+
+            // Create the final solution.
+            solution = new ShortestPathSolution(data, Status.OPTIMAL, new Path(graph, arcs));
+        }
+        
         return solution;
     }
 
--- src/main/org/insa/algo/shortestpath/Label.java
+++ src/main/org/insa/algo/shortestpath/Label.java
@@ -3,20 +3,75 @@ package org.insa.algo.shortestpath;
 import org.insa.graph.Arc;
 import org.insa.graph.Node;
 
-public class Label {
-	private Node sommetCourant;
+public class Label implements Comparable<Label> {
+	private int sommetCourant;
 	private boolean marque;
+	private boolean visite;
 	private double cost;
 	private Arc pere;
+
 	
-	protected double getCost() {
-		return this.cost;
-	}
-	
-	public Label (Node s, boolean m, double c, Arc p) {
+	public Label (int s, boolean m, double c, Arc p) {
 		this.sommetCourant = s;
 		this.marque = m;
 		this.cost = c;
 		this.pere = p;
+	}	
+	
+	/*
+	 * setter
+	 */
+	
+	protected void setSommetCourant(int s) {
+		this.sommetCourant = s;
+	}
+	protected void setMarque(boolean b) {
+		this.marque = b;
+	}
+	protected void setCost(double c) {
+		this.cost = c;
+	}
+	protected void setPere(Arc p) {
+		this.pere = p;
+	}
+	protected void setVisite(boolean v) {
+		this.visite = v;
+	}
+	
+	/*
+	 * getter
+	 */
+	protected double getCost() {
+		return this.cost;
+	}
+	
+	protected Arc getPere() {
+		return this.pere;
+	}
+
+	protected boolean getMarque() {
+		return this.marque;
+	}	
+	
+	protected int getSommetCourant() {
+		return this.sommetCourant;
+	}
+	
+	protected boolean getVisite() {
+		return this.visite;
+	}
+	
+	public int compareTo(Label autrelabel) {
+		
+		/*int cmp = 0;
+		
+		if (this.cost<autrelabel.getCost()) {
+			cmp = -1;
+		} else if (this.cost>autrelabel.getCost()){
+			cmp = 1;
+		} else if (this.cost==autrelabel.getCost()) {
+			cmp = 0;
+		}*/
+		return ((Double)(this.cost)).compareTo((Double)(autrelabel.getCost()));
 	}
 }
